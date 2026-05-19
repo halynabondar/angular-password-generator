97168c13ba71708664e33b002e7ea4ca2fe3c392
Halyna Bondar <104671539+halynabondar@users.noreply.github.com>
2026-05-19 19:39:05 +0200

    Setup Angular password generator structure

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,10 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { PasswordGenerator } from './components/password-generator/password-generator';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [PasswordGenerator],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App {
-  protected readonly title = signal('password-generator');
-}
+export class App {}
---
+++ src/app/components/password-generator/password-generator.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-password-generator',
+  imports: [],
+  templateUrl: './password-generator.html',
+  styleUrl: './password-generator.css',
+})
+export class PasswordGenerator {}
