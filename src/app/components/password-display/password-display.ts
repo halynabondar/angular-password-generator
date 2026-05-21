4042aa542ab9ef6f830f6ea76b13fa79cfed6517
Halyna Bondar <104671539+halynabondar@users.noreply.github.com>
2026-05-21 10:51:26 +0200

    Add snackbar for copy action

--- src/app/components/password-display/password-display.ts
+++ src/app/components/password-display/password-display.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { Copy, LucideAngularModule, RefreshCw } from 'lucide-angular';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-password-display',
-  imports: [LucideAngularModule],
+  imports: [LucideAngularModule, MatSnackBarModule],
   templateUrl: './password-display.html',
   styleUrl: './password-display.css',
 })
 export class PasswordDisplay {
   protected readonly RefreshCw = RefreshCw;
   protected readonly Copy = Copy;
+
+  password = 'X9!pL#m7Q@kR3$tZ';
+
+  constructor(private snackBar: MatSnackBar) {}
+
+  copyPassword() {
+    navigator.clipboard.writeText(this.password);
+
+    this.snackBar.open('Copied to clipboard', 'Close', {
+      duration: 3000,
+    })
+  }
 }
